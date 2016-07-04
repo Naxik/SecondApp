55afa8eb45ab6a94f9a1128ff69e6c8457bc63a2
Naxxer <polimorph123456@gmail.com>
2016-07-04 15:58:29 +0300

    Refactor code

--- src/main/java/ru/naxxer/web/controller/rest/api/ApplicationController.java
+++ src/main/java/ru/naxxer/web/controller/rest/api/ApplicationController.java
@@ -1,4 +1,4 @@
-package ru.naxxer.controller.rest.api;
+package ru.naxxer.web.controller.rest.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
