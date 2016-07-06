6f9514ebae26b0135a2a3e00fdce6603fdb5ef83
Naxxer <polimorph123456@gmail.com>
2016-07-06 11:36:44 +0300

    Fix mapping for index.hml page

--- src/main/java/ru/naxxer/web/controller/IndexController.java
+++ src/main/java/ru/naxxer/web/controller/IndexController.java
@@ -2,18 +2,16 @@ package ru.naxxer.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author naxxer
- *         date: 05.07.16.
+ *         date: 06.07.16.
  */
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public ModelAndView index() {
-        return new ModelAndView("index");
+    @RequestMapping("/")
+    public String index() {
+        return "forward:html/index.html";
     }
 }
